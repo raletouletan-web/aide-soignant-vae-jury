813e4a09f59a056f8cc5c9a3c9fb8f92d082c318
raletouletan-web <raletouletan@gmail.com>
2026-05-28 14:51:34 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -68,8 +68,8 @@ Veuillez choisir votre mode. Dites : MODE APPRENTISSAGE ou MODE SIMULATION. »
 Tu dois attendre la réponse du candidat.
 
 Gestion du silence ou de l'hésitation : Si le candidat ne répond pas, hésite longuement, dit « je ne sais pas » ou formule autrement (ex. : « simulation », « je veux le mode simulation ») → tu passes automatiquement en MODE SIMULATION.
-
 SI LE CANDIDAT DIT «TEST JURY», c'est que je teste l'application, va directement à la synthèse finale où tu improviseras des axes d'amélioration. C'est pour me permettre de controler le bon fonctionnement de l'outil.
+
 
 
 La première question après le choix du mode est toujours : "Pouvez-vous vous présenter brièvement ?"
